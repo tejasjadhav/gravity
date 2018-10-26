33053e5aaa2b9e5bb3515c40516c5ffdb50b4493
Tejas Jadhav <developer.tejas.jadhav@gmail.com>
2018-10-27 02:03:48 +0530

    ✨ Add CSS support and integrate CSS reboot

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,52 @@
-const path = require('path');
+const path = require("path");
+
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 function buildConfig(env, argv) {
-  const isDevMode = argv.mode !== 'production';
+  console.info(`Running webpack in ${argv.mode} mode...`);
 
   const config = {
-    entry: path.join(__dirname, '/src/index.ts'),
+    devtool: "source-map",
+    entry: path.join(__dirname, "/src/index.ts"),
     output: {
-      filename: 'gravity.js',
-      path: path.resolve(__dirname, 'dist')
+      filename: "gravity.js",
+      path: path.resolve(__dirname, "dist")
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
+          loader: "ts-loader",
+          exclude: /node_modules/
         },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            { loader: "css-loader" }
+          ],
+          exclude: /node_modules/
+        }
       ]
     },
     resolve: {
-      extensions: ['.tsx', '.ts', '.js']
+      extensions: [".tsx", ".ts", ".js"]
     },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "gravity.css"
+      }),
+      new CleanWebpackPlugin([path.resolve(__dirname, "dist")]),
+      new HtmlWebpackPlugin({
+        filename: path.resolve(__dirname, "dist/index.html"),
+        template: path.resolve(__dirname, "public/index.html"),
+        xhtml: true
+      })
+    ]
   };
 
   return config;
