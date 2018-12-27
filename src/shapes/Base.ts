82defbe8720654a0e2e4f8551de4f8796684ede2
Tejas Jadhav <developer.tejas.jadhav@gmail.com>
2018-12-27 12:29:19 +0530

    🎉 Add basic shape interfaces

---
+++ src/shapes/Base.ts
@@ -0,0 +1,16 @@
+export interface BoundingBox2D {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+export default abstract class BaseShape {
+  abstract x: number;
+  abstract y: number;
+  abstract width: number;
+  abstract height: number;
+
+  abstract getBoundingBox(): BoundingBox2D;
+  abstract draw(ctx: CanvasRenderingContext2D): void;
+}
---
+++ src/shapes/Rectangle.ts
@@ -0,0 +1,46 @@
+import BaseShape, { BoundingBox2D } from "./Base";
+
+export default class Rectangle extends BaseShape {
+  public x: number;
+  public y: number;
+  public width: number;
+  public height: number;
+  private color: string;
+
+  constructor(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    color: string = "#000000"
+  ) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  public getBoundingBox(): BoundingBox2D {
+    const halfWidth = this.width / 2;
+    const halfHeight = this.height / 2;
+
+    return {
+      x1: this.x - halfWidth,
+      y1: this.x - halfHeight,
+      x2: this.x + halfWidth,
+      y2: this.x + halfHeight
+    };
+  }
+
+  public draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+  }
+}
